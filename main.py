a80a6602d8b7a48b0135ee37171b8b76e05079ba
MatheusMDi <matheus.diniz20.mm@gmail.com>
2024-01-29 22:32:02 -0300

    Link entre CPF e multiplas contas bancarias

--- main.py
+++ main.py
@@ -1,68 +1,53 @@
 from datetime import datetime
 
-banco = 'Diniz National Bank (DNB)'
+class Movimentacao:
+    def __init__(self, tipo, valor):
+        self.tipo = tipo
+        self.valor = valor
+        self.data = datetime.now()
 
-def realizar_saque(saldo, qtd_limite_saque, VALOR_LIMITE_SAQUE, historico_saques):
-    valor_saque = int(input('Valor do saque: R$ '))
+    def __str__(self):
+        return f"{self.tipo} de R$ {self.valor:.2f} em {self.data.strftime('%d/%m/%Y %H:%M:%S')}"
 
-    if qtd_limite_saque > 0 and valor_saque <= VALOR_LIMITE_SAQUE and valor_saque <= saldo:
-        qtd_limite_saque -= 1
-        saldo -= valor_saque
-        print('                SUCESSO!')
-        print(f'       Saque de R$ {valor_saque:.2f} realizado')
+class Conta:
+    def __init__(self, agencia, numero_conta, usuario):
+        self.agencia = agencia
+        self.numero_conta = numero_conta
+        self.usuario = usuario
+        self.saldo = 0.0
+        self.movimentacoes = []
 
-        historico_saques.append(valor_saque)
-    else:
-        print('                  ERRO!')
-        if qtd_limite_saque == 0:
-            print('        Limite de saques atingido')
-        elif valor_saque > VALOR_LIMITE_SAQUE:
-            print('       Valor de saque indisponível')
+    def realizar_saque(self, valor):
+        if valor > 0 and valor <= self.saldo:
+            self.saldo -= valor
+            movimentacao = Movimentacao('Saque', valor)
+            self.movimentacoes.append(movimentacao)
+            return True
         else:
-            print('      Valor indisponível para saque')
+            print('ERRO: Valor de saque indisponível')
+            return False
 
-    print('\nRetornando ao menu...')
-    return saldo, qtd_limite_saque, historico_saques
+    def realizar_deposito(self, valor):
+        if valor > 0:
+            self.saldo += valor
+            movimentacao = Movimentacao('Depósito', valor)
+            self.movimentacoes.append(movimentacao)
+            return True
+        else:
+            print('ERRO: Valor de depósito inválido')
+            return False
 
-def realizar_deposito(saldo, historico_depositos):
-    valor_deposito = int(input('Valor do depósito: R$ '))
+    def consultar_extrato(self):
+        print(f'\nExtrato da conta {self.agencia}-{self.numero_conta} ({self.usuario.nome}):\n')
+        if len(self.movimentacoes) == 0:
+            print('Nenhuma movimentação realizada.')
+        else:
+            for movimentacao in self.movimentacoes:
+                print(movimentacao)
+            print(f'Saldo atual: R$ {self.saldo:.2f}')
 
-    if valor_deposito > 0:
-        saldo += valor_deposito
-        print('\n                SUCESSO!')
-        print(f'      Depósito de R$ {valor_deposito:.2f} realizado')
-        historico_depositos.append(valor_deposito)
-    else:
-        print('\nValor de depósito inválido')
-
-    print('\nRetornando ao menu...')
-    return saldo, historico_depositos
-
-def consultar_extrato(banco, saldo, historico_saldos, historico_depositos, historico_saques, qtd_limite_saque):
-    print(f'\n{banco:^41}'.upper())
-    print('-' * 41)
-    print('\n            Extrato da conta\n')
-
-    if len(historico_depositos) == 0 and len(historico_saques) == 0:
-        print('       Nenhuma operação realizada')
-        print('-' * 41)
-    else:
-        print(f'Saldo inicial: R$ {historico_saldos[0]:.2f}\n')
-
-    if len(historico_depositos) > 0:
-        print('depósitos'.upper())
-        for deposito in historico_depositos:
-            print(f'R$ {deposito:.2f}')
-        print('-' * 41)
-
-    if len(historico_saques) > 0:
-        print('saques'.upper())
-        for saque in historico_saques:
-            print(f'R$ {saque:.2f}')
-        print('-' * 41)
-
-    print(f'Saldo atual da conta: R$ {saldo:.2f}')
-    print(f'Saques diários restantes: {qtd_limite_saque}\n')
+    def __str__(self):
+        return f"Agência: {self.agencia}\nNúmero da Conta: {self.numero_conta}\nSaldo: R$ {self.saldo:.2f}"
 
 class Usuario:
     def __init__(self, nome, cpf, data_nascimento, endereco):
@@ -78,17 +63,6 @@ class Usuario:
 
     def __str__(self):
         return f"Nome: {self.nome}\nCPF: {self.cpf}\nData de Nascimento: {self.data_nascimento}\nEndereço: {self.endereco}"
-
-class Conta:
-    def __init__(self, agencia, numero_conta, usuario):
-        self.agencia = agencia
-        self.numero_conta = numero_conta
-        self.usuario = usuario
-        self.saldo = 0.0
-
-    def __str__(self):
-        return f"Agência: {self.agencia}\nNúmero da Conta: {self.numero_conta}\nSaldo: R$ {self.saldo:.2f}\n{str(self.usuario)}"
-
 usuarios = []
 
 def criar_usuario():
@@ -145,62 +119,147 @@ def listar_contas():
     else:
         print("\nUsuário não encontrado.\n")
 
-def exibir_menu():
+def acessar_conta():
+    cpf = input("Digite o CPF do usuário para acessar a conta: ")
+    usuario = next((u for u in usuarios if u.cpf == cpf), None)
+
+    if usuario:
+        conta_existente = input("Digite o número da conta existente no formato AGENCIA-NUMERO: ")
+        conta = next((c for c in usuario.contas if f"{c.agencia}-{c.numero_conta}" == conta_existente), None)
+
+        if conta:
+            return conta
+        else:
+            print("Conta não encontrada.")
+            return None
+    else:
+        print("Usuário não encontrado.")
+        return None
+
+def exibir_menu_inicial():
     return '''         Seja bem-vindo ao DNB!
+[A] Acessar uma conta existente
+[C] Criar uma nova conta
+[L] Listar dados completos das contas por CPF
+[T] Listar todas as contas (dados bancários detalhados)
+[M] Adicionar conta associada a CPF existente
+[Q] Sair
+
+Selecione uma opção: '''
+
+def listar_dados_completos_contas():
+    cpf = input("Digite o CPF do usuário para listar todas as contas: ")
+
+    # Procurar o usuário pelo CPF
+    usuario = next((u for u in usuarios if u.cpf == cpf), None)
+
+    if usuario:
+        print(f"\nDados completos das contas do usuário {usuario.nome} ({usuario.cpf}):\n")
+        for conta in usuario.contas:
+            print(conta)
+            print('-' * 30)
+    else:
+        print("\nUsuário não encontrado.\n")
+
+def exibir_menu_operacoes():
+    return '''         Operações da Conta
 [S] Sacar
 [D] Depositar
-[E] Consultar extrato
-[C] Criar Usuário
-[CC] Criar Conta
-[LC] Listar Contas
-[Q] Sair
+[E] Consultar Extrato
+[V] Voltar para o Menu Inicial
 
 Selecione uma operação: '''
 
-def main():
-    opcao = 'Início'
-    qtd_limite_saque = 3
-    VALOR_LIMITE_SAQUE = 500.0
-    saldo = 1500.0
-    valor_saque = valor_deposito = 0
-    historico_depositos = []
-    historico_saques = []
-    historico_saldos = [saldo]
+def adicionar_conta_existente():
+    cpf = input("Digite o CPF do usuário para associar a conta existente: ")
 
+    # Procurar o usuário pelo CPF
+    usuario = next((u for u in usuarios if u.cpf == cpf), None)
+
+    if usuario:
+        agencia = input("Agência: ")
+        numero_conta = input("Número da Conta: ")
+
+        # Verificar se o número da conta já está em uso
+        while any(conta.numero_conta == numero_conta for conta in usuario.contas):
+            print("Número da conta já em uso. Tente novamente.")
+            numero_conta = input("Número da Conta: ")
+
+        usuario.adicionar_conta(agencia, numero_conta)
+        print("\nConta associada com sucesso!\n")
+    else:
+        print("\nUsuário não encontrado. Por favor, crie um usuário primeiro.\n")
+
+
+def listar_todas_contas():
+    print("\nTodas as contas com dados bancários detalhados:\n")
+    for usuario in usuarios:
+        print(f"Usuário: {usuario.nome} ({usuario.cpf})")
+        for conta in usuario.contas:
+            print(f"\n{conta}")
+            print('-' * 30)
+
+def realizar_operacoes_conta(banco, conta):
     while True:
         print('-' * 41)
         print(f'{banco:^41}'.upper())
         print('-' * 41)
-        opcao = str(input(exibir_menu())).upper()
+        opcao = str(input(exibir_menu_operacoes())).upper()
         print('-' * 41)
 
         if opcao == 'S':
-            saldo, qtd_limite_saque, historico_saques = realizar_saque(saldo, qtd_limite_saque, VALOR_LIMITE_SAQUE,
-                                                                       historico_saques)
+            valor_saque = float(input('Digite o valor do saque: R$ '))
+            if conta.realizar_saque(valor_saque):
+                print('Saque realizado com sucesso!')
 
         elif opcao == 'D':
-            saldo, historico_depositos = realizar_deposito(saldo, historico_depositos)
+            valor_deposito = float(input('Digite o valor do depósito: R$ '))
+            if conta.realizar_deposito(valor_deposito):
+                print('Depósito realizado com sucesso!')
 
         elif opcao == 'E':
-            consultar_extrato(banco, saldo, historico_saldos, historico_depositos, historico_saques, qtd_limite_saque)
+            conta.consultar_extrato()
 
-        elif opcao == 'C':
-            criar_usuario()
-
-        elif opcao == 'CC':
-            criar_conta()
-
-        elif opcao == 'LC':
-            listar_contas()
-
-        elif opcao == 'Q':
-            print('      O GNB agradece a preferência!')
-            print()
+        elif opcao == 'V':
+            print('Retornando ao Menu Inicial...')
             break
 
         else:
-            print('   Operação inválida. Tente novamente')
+            print('Operação inválida. Tente novamente.')
+            print()
+
+def main(banco):
+    while True:
+        print("-" * 41)
+        print(f"{banco:^41}".upper())
+        print("-" * 41)
+        opcao_inicial = str(input(exibir_menu_inicial())).upper()
+        print("-" * 41)
+
+        if opcao_inicial == 'A':
+            conta = acessar_conta()
+            if conta:
+                realizar_operacoes_conta(banco, conta)
+        elif opcao_inicial == 'C':
+            criar_usuario()
+            criar_conta()
+        elif opcao_inicial == 'L':
+            listar_dados_completos_contas()
+
+        elif opcao_inicial == 'T':
+            listar_todas_contas()
+
+        elif opcao_inicial == 'M':
+            adicionar_conta_existente()
+
+        elif opcao_inicial == 'Q':
+            print("O DNB agradece a preferência!")
+            print()
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
             print()
 
 if __name__ == "__main__":
-    main()
+    banco = 'Diniz National Bank (DNB)'
+    main(banco)
